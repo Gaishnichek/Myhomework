4df443947d902a704441c94de0ff6cef141d2408
Gaishnichek <gaishnichek@icloud.com>
2022-11-17 10:53:51 +0200

    trsa lz lz

--- main.py
+++ main.py
@@ -15,5 +15,34 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+# types
+
+# int, float
+
+# bool
+
+# str
+
+name="Stas"
+age= 29
+
+my_str= "Привіт! Мене звати %s, мені %s років" % (name, age)
+print(my_str)
+tpl="Привіт! Мене звати {}, мені {} років"
+my_str=  tpl.format (name, age)
+print(my_str)
 
 
+tpl="Привіт! Мене звати {name_var}, мені {age_var} років"
+my_str=  tpl.format (name_var=name, age_var=age)
+print(my_str)
+
+# f-sting
+my_str=f"Привіт! Мене звати {name}, мені {age} років"
+print(my_str)
+
+print("Hello" in my_str)
+
+print("Привіт" in my_str)
+
+my_str=f"Привіт! Мене звати {name}, мені {age} років"
